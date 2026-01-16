c151a9426777b74679d8944956ae35051b9d26e1
AMRITHA PD <amrithapd977@gmail.com>
2026-01-17 02:21:18 +0530

    Update model.py

--- backend/model.py
+++ backend/model.py
@@ -1,6 +1,10 @@
 import joblib
+import os
 
-model = joblib.load("learning_risk_model.pkl")
+BASE_DIR = os.path.dirname(os.path.abspath(__file__))
+MODEL_PATH = os.path.join(BASE_DIR, "learning_risk_model.pkl")
+
+model = joblib.load(MODEL_PATH)
 
 def predict(features):
     return model.predict([features])[0]
