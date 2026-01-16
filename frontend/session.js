974b1fb5f504cc1354f8333b7727412ba42d4a6b
AMRITHA PD <amrithapd977@gmail.com>
2026-01-17 01:50:14 +0530

    Create session.js

---
+++ frontend/session.js
@@ -0,0 +1,12 @@
+const sessionState = {
+  metrics: {
+    reaction_time_mean: 0,
+    reaction_time_std: 0,
+    reading_wpm: 0,
+    regressions: 0,
+    phoneme_errors: 0,
+    weber_fraction: 0,
+    math_decay: 0,
+    switch_latency: 0
+  }
+};
