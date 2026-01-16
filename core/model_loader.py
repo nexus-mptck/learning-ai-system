41510d576938c024648ce87f66e4926e4b491ae8
AMRITHA PD <amrithapd977@gmail.com>
2026-01-17 01:43:56 +0530

    Delete core directory

--- core/model_loader.py
+++
@@ -1,9 +0,0 @@
-import joblib
-from core.config import MODEL_PATH
-
-model = joblib.load(MODEL_PATH)
-
-def predict_risk(features):
-    prediction = model.predict([features])[0]
-    probabilities = model.predict_proba([features])
-    return prediction, probabilities
